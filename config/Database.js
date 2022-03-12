b33eb92effb9eb6dc6de98af60e93c665d9f1635
Ujjwal Gupta <ujjwalg056@gmial.com>
2022-03-12 21:25:34 +0530

    updating for deployment, made string for mongoose.connect #02

--- config/Database.js
+++ config/Database.js
@@ -9,7 +9,7 @@ class Database {
     connect () {
         console.log("Connecting to database...");
 
-        mongoose.connect(process.env.MONGO_URI, {
+        mongoose.connect(String(process.env.MONGO_URI), {
             useNewUrlParser: true,
             useUnifiedTopology: true,
 
